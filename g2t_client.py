973b0385fa13fc74f63a7ddad36bb4b6c8744e80
Daniel-M <danielmejia55@gmail.com>
2018-03-23 15:46:28 -0500

    Updated client

--- g2t_client.py
+++ g2t_client.py
@@ -20,12 +20,22 @@ class GDAL2TilesSpawner():
         # Kills child process
         self.kill_process()
 
-    def __init__(self, layer_id, image, profile="mercator", zoom="15-22", alpha="0,0,0",
-                 progress_callback=None, done_callback=None, timeout=1800 ,binary="gdal2tiles_mp.py"
-                 , python_bin="python3", **kwargs):
+    def __init__(self,
+                 layer_id,
+                 image,
+                 profile="mercator",
+                 zoom="15-22",
+                 alpha="0,0,0",
+                 progress_callback=None,
+                 done_callback=None,
+                 timeout=1800,
+                 binary="gdal2tiles_mp.py",
+                 python_bin="python3",
+                 **kwargs):
         """
         __init__ populates the class members. The method options are,  
 
+        layer_id:(int) layer id of the image to be processed.  
         image:(string) full path to the image.  
         profile:(string) the projection profile.(default "mercator")(options:
         "geodetic" "mercator").  
@@ -34,6 +44,9 @@ class GDAL2TilesSpawner():
         progress_callback:(function) a function that receives a single argument.
                           the purpose is to report the progress of the tiling.
                           (default None)
+        done_callback:(function) a function that receives a single argument.
+                          the purpose is to report the "Done" status of the
+                          tiler.(default None)
         timeout:(int) time in seconds to wait before killing the spawned process
                 useful for garbage collecting stalled processes.(default 1800
                 seconds)
@@ -46,12 +59,12 @@ class GDAL2TilesSpawner():
         subprocess.   
 
         """
+        self.layer_id = layer_id 
         self.image = image
         self.profile = profile
         self.alpha = alpha
         self.progress_callback = progress_callback # progress_callback function
         self.done_callback = done_callback # done_callback function
-        self.layer_id = layer_id 
 
         self.timeout = timeout
         self.binary = binary
@@ -85,6 +98,7 @@ class GDAL2TilesSpawner():
     def mk_log(self):
         logging.info("GDAL2TilesSpawner PROCESS SPAWNED")
         logging.info("pid: " + str(self.process.pid))
+        logging.info("layer_id" + str(self.layer_id))
         logging.info("image: " + self.image)
         logging.info("profile: " + self.profile)
         logging.info("alpha: " + str(self.alpha)) # alpha should be a tuple
@@ -92,15 +106,27 @@ class GDAL2TilesSpawner():
         logging.info("command: " + str(self.arglist))
    
     def mk_args(self):
-        argstr = self.python_bin + " " + self.binary 
-        argstr +=  " --profile="+ self.profile + " -a "
-        argstr +=  self.alpha + " --zoom " + self.zoom_min+"-" 
-        argstr +=  self.zoom_max + " " + self.image + " -w none "
+        argstr = self.python_bin
+        argstr += " " + self.binary 
+        argstr +=  " --profile="+ self.profile
+        argstr +=  " -a " + self.alpha
+        
+        # Process the zooms for arguments
+        if self.zoom_max != self.zoom_min:
+            argstr +=  " --zoom " + self.zoom_min + "-" + self.zoom_max
+        else:
+            argstr +=  " --zoom " + self.zoom_min
+
+        # Don't generate html files
+        argstr +=  " -w none"
+        argstr +=  " " + self.image
 
         if self.kwargs:
             if 'output' in self.kwargs:
+                argstr += " " 
                 argstr += self.kwargs["output"]
 
+        # Generate lexically valid argument list
         self.arglist = shlex.split(argstr)
 
     def __call__(self):
@@ -113,6 +139,7 @@ class GDAL2TilesSpawner():
         self.process = subprocess.Popen(self.arglist, stdout=subprocess.PIPE) # Spawn process 
 
         spawned_processes[self.process.pid] = {"pid": self.process.pid,
+                                               "layer_id": self.layer_id,
                                                "image": self.image,
                                                "profile" : self.profile,
                                                "alpha" : str(self.alpha), # alpha should be a tuple
@@ -158,8 +185,6 @@ class GDAL2TilesSpawner():
         self.mk_log()
 
         res = self.process.wait(timeout=self.timeout)
-       
-        res = self.process.wait(timeout=self.timeout)
 
         if res < 0:
             logging.warning("process with pid " + str(self.process.pid) + "terminated with exit code "+ str(res))
@@ -190,8 +215,8 @@ class GDAL2TilesSpawner():
 
 ###############################################################################
 
-def main(image, profile, zoom, alpha, timeout=1800):
-    G2T = GDAL2TilesSpawner(image, profile, zoom, alpha, timeout)
+def main(layer_id, image, profile, zoom, alpha, timeout=1800):
+    G2T = GDAL2TilesSpawner(layer_id, image, profile, zoom, alpha, timeout)
 
     # If it can't run, kill all the spawned processes
     try:
@@ -207,6 +232,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Spawn GDAL2Tiles processes")
 
     ## Add the list of arguments
+    parser.add_argument('-l', '--layer_id', help="Layer ID of the image processed", type=str)
     parser.add_argument('-i', '--image', help="Path to the image where the plants will be counted.", type=str)
     parser.add_argument('-t', '--timeout', help="tolerance for the color detection.", type=float)
     parser.add_argument('-a', '--alpha', help="Circle specification [center_x,center_y,radious].", type=str)
@@ -217,7 +243,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Check that the arguments passed in good shape
-    if not None in [args.image, args.zoom, args.profile, args.alpha]:
-        main(args.image,  args.profile, args.zoom, args.alpha)
+    if not None in [args.layer_id, args.image, args.zoom, args.profile, args.alpha]:
+        main(args.layer_id, args.image,  args.profile, args.zoom, args.alpha)
     else:
         print("Wrong set of parameters")
